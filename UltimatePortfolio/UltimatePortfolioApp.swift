e2c33ffb620a5a5b28e520157f27ef221f1bd4ac
Dmitriy Bronnikov <dbron9269@gmail.com>
2023-11-25 02:07:42 +0200

    Initial Commit

---
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolioApp.swift
+//  UltimatePortfolio
+//
+//  Created by Dmitriy Bronnikov on 25.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
